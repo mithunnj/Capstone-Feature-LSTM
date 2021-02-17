ab8e39ff905c3f622078c68ed42ab681c2f83162
Mithun Jothiravi <mithun.jothiravi@mail.utoronto.ca>
2021-02-17 16:25:22 -0500

    compute_features.py
    Included a compute_physics_features to compute x,y, vel_x, vel_y, acc_x,
    acc_y, jerk_x, jerk_y for all agents given a .csv data file. When the
    compute_features.py script is called with the following sample call:
    python compute_features.py --data_dir ./data/forecasting_sample/data
    --feature_dir ./computed_features --mode train, for each .csv file, a
    data structure will store all the per agent physics information, and
    save it in a .pkl file in the ./computed_features directory with the
    following filepath: computed_physics_features_file_<csv_filename>.pkl
    
    - Sample physics calculation:
        vel_x = (x2-x1)/(t2-t1)
    
    - ./data: This directory will be used to store data, but as described in
      the .gitignore, it is on the developer to download and store the data
      in this directory. All the contents of the ./data directory will be
      ignored.
    - computed_features: This directory contains the computed features .pkl
      files as defined in the --feature_dir argument of the
      compute_features.py script call.

--- compute_features.py
+++ compute_features.py
@@ -3,6 +3,7 @@
 Example usage:
     $ python compute_features.py --data_dir ~/val/data 
         --feature_dir ~/val/features --mode val
+    $ python compute_features.py --data_dir ./data/forecasting_sample/data --feature_dir ./computed_features --mode train
 """
 
 import os
@@ -10,6 +11,7 @@ import shutil
 import tempfile
 import time
 from typing import Any, Dict, List, Tuple
+import sys
 
 import argparse
 from joblib import Parallel, delayed
@@ -20,6 +22,11 @@ import pickle as pkl
 from utils.baseline_config import RAW_DATA_FORMAT, _FEATURES_SMALL_SIZE
 from utils.map_features_utils import MapFeaturesUtils
 from utils.social_features_utils import SocialFeaturesUtils
+
+# Set global FPs
+CURR_DIR = os.getcwd()
+DATA_DIR = CURR_DIR + "/data"
+COMPUTE_FEATURES_SAVE_DIR = CURR_DIR + "/computed_features"
 
 
 def parse_arguments() -> Any:
@@ -86,6 +93,7 @@ def load_seq_save_features(
     args = parse_arguments()
     data = []
 
+    
     # Enumerate over the batch starting at start_idx
     for seq in sequences[start_idx:start_idx + args.batch_size]:
 
@@ -94,6 +102,9 @@ def load_seq_save_features(
 
         file_path = f"{args.data_dir}/{seq}"
         seq_id = int(seq.split(".")[0])
+
+        # Compute physics features
+        compute_physics_features(file_path)
 
         # Compute social and map features
         features, map_feature_helpers = compute_features(
@@ -115,6 +126,8 @@ def load_seq_save_features(
         print(
             f"{args.mode}:{count}/{args.batch_size} with start {start_idx} and end {start_idx + args.batch_size}"
         )
+        
+        break # DEBUG REMOVE - Wanted to break out of this process after a single file
 
     data_df = pd.DataFrame(
         data,
@@ -137,6 +150,72 @@ def load_seq_save_features(
         f"{save_dir}/forecasting_features_{args.mode}_{start_idx}_{start_idx + args.batch_size}.pkl"
     )
 
+def compute_physics_features(seq_path):
+
+    # Helper function to calculate time varying parameters for agents
+    def rate_of_change(data, param, index):
+        param_change = data[index][param] - data[index-1][param]
+        time_change = data[index]['TIMESTAMP'] - data[index-1]['TIMESTAMP']
+
+        return param_change/time_change
+
+    df = pd.read_csv(seq_path) # Load .csv data
+    # Per agent information 
+    per_agent_info_headings = ["TIMESTAMP", "TRACK_ID", "X", "Y", "VEL_X", "VEL_Y", "ACC_X", "ACC_Y", "JERK_X", "JERK_Y"] 
+
+    # Get all unique track_ids (represents the agents in the scene - .csv file)
+    agent_ids = df["TRACK_ID"].unique().tolist()
+
+    # Data store for .csv file
+    total_data = dict()
+
+    for agent in agent_ids:
+
+        agent_info = list() # List to contain the agent information over a series of timesteps
+        agent_df = df[df['TRACK_ID'] == agent] # Agent specific data from the .csv dataframe
+            
+        for index, row in agent_df.iterrows(): # Each row represents a specific timestep of data for a specific agent (track_id)
+            
+            data = {key: None for key in per_agent_info_headings} # Initialize empty data structure to store row data
+
+            # Load data
+            data['TIMESTAMP'] = row['TIMESTAMP']
+            data['TRACK_ID'] = row['TRACK_ID']
+            data["X"] = row["X"]
+            data["Y"] = row["Y"]
+
+            # Store agent data
+            agent_info.append(data)
+
+            # Update time varying data
+            index = 0 if len(agent_info)  == 0 else len(agent_info)-1
+
+            # Update velocity after there are at least 2 position information
+            if len(agent_info) >= 2:
+                agent_info[index]["VEL_X"] = rate_of_change(agent_info, "X", index)
+                agent_info[index]["VEL_Y"] = rate_of_change(agent_info, "Y", index)
+
+            # Update acceleration after there are at least 2 velocity information
+            if len(agent_info) >= 3:
+                agent_info[index]["ACC_X"] = rate_of_change(agent_info, "VEL_X", index)
+                agent_info[index]["ACC_Y"] = rate_of_change(agent_info, "VEL_Y", index)
+
+            # Update jerk after there are at least 2 acceleration information
+            if len(agent_info) >= 4:
+                agent_info[index]["JERK_X"] = rate_of_change(agent_info, "ACC_X", index)
+                agent_info[index]["JERK_Y"] = rate_of_change(agent_info, "ACC_Y", index)
+
+        total_data[agent] = agent_info # Store all timestep'd data for this agent
+
+    # Save data to .pkl file in the COMPUTED_FEATURES_DIR 
+    file_name = "computed_physics_features_file_{}.pkl".format(seq_path.split('/')[-1].split('.')[0])
+    save_dir = COMPUTE_FEATURES_SAVE_DIR + "/{}".format(file_name)
+    pkl_file = open(save_dir, "wb+")
+    pkl.dump(total_data, pkl_file)
+    pkl_file.close()
+
+    return
+
 
 def compute_features(
         seq_path: str,
@@ -154,6 +233,7 @@ def compute_features(
         map_feature_helpers (dict): Dictionary containing helpers for map features
 
     """
+   
     args = parse_arguments()
     df = pd.read_csv(seq_path, dtype={"TIMESTAMP": str})
 
