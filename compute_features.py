71c8419db8a7a3fc6dabd3a6bff4b1ff7ee9460d
Mithun Jothiravi <mithun.jothiravi@mail.utoronto.ca>
2021-02-18 11:40:25 -0500

    Ported over compute_physics_features to dedicated utils directory located at ./utils/compute_features.py
    - calc_magnitude() func: Given the x,y vectors, this will calculate the
      magnitude.
    
    - compute_physics_features() func: Given the filepath of a .csv data
      file (handled by Argoverse utils), the following physics features will
      calculated for each agent in the .csv data file and stored in the
      ./computed_features directory: timestamp, track_id, x, y, vel_x,
      vel_y, acc_x, acc_y, jerk_x, jerk_y

--- compute_features.py
+++ compute_features.py
@@ -22,12 +22,7 @@ import pickle as pkl
 from utils.baseline_config import RAW_DATA_FORMAT, _FEATURES_SMALL_SIZE
 from utils.map_features_utils import MapFeaturesUtils
 from utils.social_features_utils import SocialFeaturesUtils
-
-# Set global FPs
-CURR_DIR = os.getcwd()
-DATA_DIR = CURR_DIR + "/data"
-COMPUTE_FEATURES_SAVE_DIR = CURR_DIR + "/computed_features"
-
+from utils.compute_features_utils import compute_physics_features
 
 def parse_arguments() -> Any:
     """Parse command line arguments."""
@@ -93,7 +88,6 @@ def load_seq_save_features(
     args = parse_arguments()
     data = []
 
-    
     # Enumerate over the batch starting at start_idx
     for seq in sequences[start_idx:start_idx + args.batch_size]:
 
@@ -127,8 +121,6 @@ def load_seq_save_features(
             f"{args.mode}:{count}/{args.batch_size} with start {start_idx} and end {start_idx + args.batch_size}"
         )
         
-        break # DEBUG REMOVE - Wanted to break out of this process after a single file
-
     data_df = pd.DataFrame(
         data,
         columns=[
@@ -149,73 +141,6 @@ def load_seq_save_features(
     data_df.to_pickle(
         f"{save_dir}/forecasting_features_{args.mode}_{start_idx}_{start_idx + args.batch_size}.pkl"
     )
-
-def compute_physics_features(seq_path):
-
-    # Helper function to calculate time varying parameters for agents
-    def rate_of_change(data, param, index):
-        param_change = data[index][param] - data[index-1][param]
-        time_change = data[index]['TIMESTAMP'] - data[index-1]['TIMESTAMP']
-
-        return param_change/time_change
-
-    df = pd.read_csv(seq_path) # Load .csv data
-    # Per agent information 
-    per_agent_info_headings = ["TIMESTAMP", "TRACK_ID", "X", "Y", "VEL_X", "VEL_Y", "ACC_X", "ACC_Y", "JERK_X", "JERK_Y"] 
-
-    # Get all unique track_ids (represents the agents in the scene - .csv file)
-    agent_ids = df["TRACK_ID"].unique().tolist()
-
-    # Data store for .csv file
-    total_data = dict()
-
-    for agent in agent_ids:
-
-        agent_info = list() # List to contain the agent information over a series of timesteps
-        agent_df = df[df['TRACK_ID'] == agent] # Agent specific data from the .csv dataframe
-            
-        for index, row in agent_df.iterrows(): # Each row represents a specific timestep of data for a specific agent (track_id)
-            
-            data = {key: None for key in per_agent_info_headings} # Initialize empty data structure to store row data
-
-            # Load data
-            data['TIMESTAMP'] = row['TIMESTAMP']
-            data['TRACK_ID'] = row['TRACK_ID']
-            data["X"] = row["X"]
-            data["Y"] = row["Y"]
-
-            # Store agent data
-            agent_info.append(data)
-
-            # Update time varying data
-            index = 0 if len(agent_info)  == 0 else len(agent_info)-1
-
-            # Update velocity after there are at least 2 position information
-            if len(agent_info) >= 2:
-                agent_info[index]["VEL_X"] = rate_of_change(agent_info, "X", index)
-                agent_info[index]["VEL_Y"] = rate_of_change(agent_info, "Y", index)
-
-            # Update acceleration after there are at least 2 velocity information
-            if len(agent_info) >= 3:
-                agent_info[index]["ACC_X"] = rate_of_change(agent_info, "VEL_X", index)
-                agent_info[index]["ACC_Y"] = rate_of_change(agent_info, "VEL_Y", index)
-
-            # Update jerk after there are at least 2 acceleration information
-            if len(agent_info) >= 4:
-                agent_info[index]["JERK_X"] = rate_of_change(agent_info, "ACC_X", index)
-                agent_info[index]["JERK_Y"] = rate_of_change(agent_info, "ACC_Y", index)
-
-        total_data[agent] = agent_info # Store all timestep'd data for this agent
-
-    # Save data to .pkl file in the COMPUTED_FEATURES_DIR 
-    file_name = "computed_physics_features_file_{}.pkl".format(seq_path.split('/')[-1].split('.')[0])
-    save_dir = COMPUTE_FEATURES_SAVE_DIR + "/{}".format(file_name)
-    pkl_file = open(save_dir, "wb+")
-    pkl.dump(total_data, pkl_file)
-    pkl_file.close()
-
-    return
-
 
 def compute_features(
         seq_path: str,
@@ -256,7 +181,6 @@ def compute_features(
     )
 
     # Combine social and map features
-
     # If track is of OBS_LEN (i.e., if it's in test mode), use agent_track of full SEQ_LEN,
     # But keep (OBS_LEN+1) to (SEQ_LEN) indexes having None values
     if agent_track.shape[0] == args.obs_len:
